c5b740e2c0ed9e8d775967bbf34dadfc869fcf02
Robert Baron <robbaron@bu.edu>
2019-01-21 15:24:44 -0500

    Fleshed out the add user a bit more

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,54 @@
+import kubernetes
+from openshift.dynamic import DynamicClient
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/projects/<project_name>")
 def create_project(project_name):
+    # "oc login -u acct-req-sa"
+    k8s_client = kubernetes.config.new_client_from_config()
+    dyn_client = DynamicClient(k8s_client)
+
+    # This is a standard Kubernetes definition for a Service
+    service = "{" 
+        + "\"kind\": \"Service\","
+        + "\"apiVersion\": \"v1\","
+        + "\"metadata\": {"
+        +     "\"name\": \"my-service\""
+        + "},"
+        + "\"spec\": {"
+        +     "\"selector\": {"
+        +         "\"app\": \"MyApp\""
+        +     "},"
+        + "\"ports\": [{"
+        +     "\"protocol\": \"TCP\","
+        +     "\"port\": 8080,"
+        +     "\"targetPort\": 9376,"
+        +     "}]"
+        +"}"
+        +"}"
+    resp = v1_services.create(body=service, namespace='acct-req')
+    # "oc create project <project_name>"
     return "{\"create project\"}"
 
 @app.route("/users/<user_name>")
 def create_user(user_name):
+    # "oc login -u acct-req-sa"
+    # "oc create user <user_name>"
+    # "oc create identity <identity_provider>:<user_name from identity provider>"
+    # "oc create useridentitymapping <identity_provider>:<user_name from identity provider> <user_name>"
+    k8s_client = kubernetes.config.new_client_from_config()
+    dyn_client = DynamicClient(k8s_client)
+    user = "{\"apiVersion\":\"v1\",\"kind\":\"User\":\"" + user_name + "\",\"identities\":[\"keystone_auth:robbaron@bu.edu\"],\"groups\":null,\"metadata\":{\"name\":\"test\"}}"
+         #  { "apiVersion": "user.openshift.io/v1", "groups": null, "identities": [ "keystone_auth:robbaron@bu.edu" ], "kind": "User", "metadata": { "name": "test" } }
+    resp = v1_services.create(body=user)
     return "{\"create user\"}"
 
 @app.route("/users/<user_name>/projects/<project_name>/roles/<role>")
 def map_project(user_name,project_name,role):
+    # "oc login -u acct-req-sa"
+    # "oc adm policy -n <project_name> add-role-to-user <role> <user_name>"
     return "{\"map\"}"
 
 if __name__ == "__main__":
