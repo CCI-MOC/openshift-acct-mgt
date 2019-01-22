3b7cf950bb8f745f4a252fae73ef465960e6d822
Robert Baron <robbaron@bu.edu>
2019-01-21 21:22:37 -0500

    squash this

--- install.pl
+++ install.pl
@@ -7,4 +7,4 @@ system("oc -n acct-req adm policy add-cluster-role-to-user cluster-admin system:
 if(open(FP,"<oc serviceaccounts get-token acct-req-sa")) { $token=<FP>; }
 system("oc login --token ".$token);
 system("oc -n acct-req create secret generic kubecfg --from-file=/root/.kube/config");
-system(" oc -n acct-req new-app python:2.7~https://github.com/robbaronbu/openshift-acct-req.git");
+system(" oc -n acct-req new-app python:3.5~https://github.com/robbaronbu/openshift-acct-req.git");
