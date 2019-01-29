d57103ac3cdfa371aa4f8a1bb2338041661227c7
robbaronbu <robbaron@bu.edu>
2019-01-29 09:06:02 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-gunicorn -b 0.0.0.0:8080 -c /app/openshift-acct-req/config.py -e PYTHONBUFFERED=TRUE wsgi:app
+gunicorn -b 0.0.0.0:8080 -c /app/openshift-acct-req/config.py -e PYTHONBUFFERED=TRUE wsgi:application
 
