1890f2e12600788a4d7fe15be3ae6671059f0d57
root <root@n-11.moclocal>
2019-02-02 10:13:00 -0500

    squash

--- wsgi.py
+++ wsgi.py
@@ -41,8 +41,10 @@ def create_user(user_name):
     k8s_client = kubernetes.config.new_client_from_config()
     dyn_client = DynamicClient(k8s_client)
     v1_users = dyn_client.resources.get(api_version='v1', kind='User')
+    print("Users: ", v1_users);
     user = "{\"apiVersion\":\"v1\",\"kind\":\"User\":\"" + user_name + "\",\"identities\":[\"keystone_auth:robbaron@bu.edu\"],\"groups\":null,\"metadata\":{\"name\":\"test\"}}"
          #  { "apiVersion": "user.openshift.io/v1", "groups": null, "identities": [ "keystone_auth:robbaron@bu.edu" ], "kind": "User", "metadata": { "name": "test" } }
+    print("Users2: ",user);
     resp = v1_users.create(body=user)
     return "{\"create user\"}"
 
