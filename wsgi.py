2e3754f9e8ab75058e2be827f2e44b90754f59f9
root <root@n-11.moclocal>
2019-02-03 12:12:00 -0500

    squash

--- wsgi.py
+++ wsgi.py
@@ -55,4 +55,7 @@ def map_project(user_name,project_name,role):
     return "{\"map\"}"
 
 if __name__ == "__main__":
+    gunicorn_logger=logging.getLogger('gunicorn.error')
+    application.logger.handlers=gunicorn_logger.handlers
+    application.logger.setLevel("info")
     application.run()
