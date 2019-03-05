511383777d44573de0332401b03f77f89a877052
Robert Baron <robbaron@bu.edu>
2019-03-05 15:49:40 -0500

    Attempting to use the rest API

--- wsgi.py
+++ wsgi.py
@@ -40,20 +40,36 @@ def create_project(project_name):
     return "{\"create project\"}"
 
 @application.route("/users/<user_name>")
-def create_user(user_name):
+@application.route("/users/<user_name>/fullname/<full_name>")
+@application.route("/users/<user_name>/fullname/<full_name>/<identity>/<id_name>")
+def create_user(user_name, full_name=null, identity="SSO_AUTH", id_name=null):
     # "oc login -u acct-req-sa"
     # "oc create user <user_name>"
     # "oc create identity <identity_provider>:<user_name from identity provider>"
     # "oc create useridentitymapping <identity_provider>:<user_name from identity provider> <user_name>"
-    pp = pprint.PrettyPrinter(indent=4)
-    k8s_client = kubernetes.config.new_client_from_config()
-    dyn_client = DynamicClient(k8s_client)
-    v1_users = dyn_client.resources.get(api_version='v1', kind='User')
-    application.logger.warning("Users: " + pp.pformat(v1_users));
-    user = "{\"apiVersion\":\"v1\",\"kind\":\"User\":\"" + user_name + "\",\"identities\":[\"keystone_auth:robbaron@bu.edu\"],\"groups\":null,\"metadata\":{\"name\":\"test\"}}"
-         #  { "apiVersion": "user.openshift.io/v1", "groups": null, "identities": [ "keystone_auth:robbaron@bu.edu" ], "kind": "User", "metadata": { "name": "test" } }
+    #pp = pprint.PrettyPrinter(indent=4)
+    #k8s_client = kubernetes.config.new_client_from_config()
+    #dyn_client = DynamicClient(k8s_client)
+    #v1_users = dyn_client.resources.get(api_version='v1', kind='User')
+    #application.logger.warning("Users: " + pp.pformat(v1_users));
+    #user = "{\"apiVersion\":\"v1\",\"kind\":\"User\":\"" + user_name + "\",\"identities\":[\"keystone_auth:robbaron@bu.edu\"],\"groups\":null,\"metadata\":{\"name\":\"test\"}}"
+    #     #  { "apiVersion": "user.openshift.io/v1", "groups": null, "identities": [ "keystone_auth:robbaron@bu.edu" ], "kind": "User", "metadata": { "name": "test" } }
     application.logger.warning("Users2: " + user);
     resp = v1_users.create(body=user)
+    token = get_user_token('/kube/config','moc-openshift-acct-req')
+    url='https://k-openshift.osh.massopen.cloud:8443/oapi/v1";
+    headers={'Authorizations': 'Bearer '+token }
+    payload={ 
+        "apiVersion":"v1",
+        "kind":"User": user_name,
+        "groups":null,
+        "metadata":
+            {
+            "name":full_name
+            }
+ 
+        }
+    json = requests.get(url, headers=headers, data=json.dumps(payload), verify=false)
     return "{\"create user\"}"
 
 @application.route("/users/<user_name>/projects/<project_name>/roles/<role>")
