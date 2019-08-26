5e548c9090aa9f4c9eee6f2d45425d19d219f6f3
Robert Baron <robbaron@bu.edu>
2019-08-26 13:41:57 -0400

    Modified the install process to handle both x86 and ppc64le arch. Still needs some refinement.

--- install.pl
+++ install.pl
@@ -1,8 +1,12 @@
 #!/usr/bin/perl
 
+#build the container
+# docker build -f Dockerfile.ppc64le -t docker.io/robertbartlettbaron/acct-mgt.ppc64le .
+# docker build -f Dockerfile.x86 -t docker.io/robertbartlettbaron/acct-mgt.x86 .
+
 # login to oc as cluster-admin
 
-# see if openshift acme client is install 
+# see if openshift acme client is install
 # if it is not, install it
 if(open(FP "oc get deployment.apps --all-namespaces |"))
     {
@@ -17,10 +21,10 @@ if(open(FP "oc get deployment.apps --all-namespaces |"))
 
 # Install moc's acct-mgt microserver
 system("oc new-project acct-mgt");
-system("oc -n acct-req create sa acct-mgt-sa");
-system("oc -n acct-req adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:acct-req:acct-req-sa");
-system("oc -n acct-req create template -f acct-req.yaml");
-system("oc create route edge ".
-       " --service=frontend ".
-       " --hostname=acct-mgt.");
+system("oc -n acct-mgt create sa acct-mgt-sa");
+system("oc -n acct-mgt adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:acct-mgt:acct-mgt-sa");
+system("oc -n acct-mgt create template -f acct-mgt.x86.yaml");
+system("oc -n acct-mgt create template -f acct-mgt.ppc64le.yaml");
+system("oc create route edge --port=443 --service=frontend --hostname=acct-mgt");
 system("oc annotate route acct-mgt kubernetes.io/tls-acme:\"true\" ");
+
