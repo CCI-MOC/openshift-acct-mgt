eeb97dfe51d8564ba0bdfc662e28a99f50416098
Robert Baron <robbaron@bu.edu>
2021-12-05 19:18:56 -0500

    mostly pylinted.

--- acct-mgt-test.py
+++ acct-mgt-test.py
@@ -1,42 +1,53 @@
 #!/usr/bin/python3
-# python3 -m pytest acct-mgt-test.py --amurl [acct_mgt_url] --user [username] --passwd [password]
-#
-# Note, to do this from apple OSX
-#    1) convert the cert and key to a p12 file
-#       via: openssl pkcs12 -export -in ./<client_cert> -inkey ./<client_key> -out client.p12
-#       openssl pkcs12 -export -in ./acct-mgt-2.crt -inkey ./acct-mgt-2.key -out acct-mgt-2.p12
-#
-#    2) call curl with
-#       curl -v -k -E ./client.p12:password http://url...
-#
-#    3) auth_opts can be of the following:
-#
-#          auth_opts = ["-E","./client_cert/acct-mgt-2.crt", "-key", "./client_cert/acct-mgt-2.key"]
-#          auth_opts = ["-cert", r"acct-mgt-2",]
-#
-# Initial test to confirm that something is working
-#    curl -kv https://acct-mgt.apps.cnv.massopen.cloud/projects/acct-mgt
-#    curl -u <user>:<password> -kv https://acct-mgt.apps.cnv.massopen.cloud/projects/acct-mgt
-#
-#  -- testing with no authentication:
-#     python3 -m pytest acct-mgt-test.py --amurl http://am2.apps.cnv.massopen.cloud
-#
-#  -- testing with basic authentication
-#     python3 -m pytest acct-mgt-test.py --amurl https://acct-mgt.apps.cnv.massopen.cloud --basic "<admin>:<password>"
+"""
+ This pytest module checks the results from a running openshift account management
+ microserver using oc (openshift's kubectl equivalent).  This is done instead of mocking
+ openshift, as openshift too frequently changes.
+
+ General commandline format:
+
+ python3 -m pytest acct-mgt-test.py --amurl [acct_mgt_url] --user [username] --passwd [password]
+
+ Note, to do this from apple OSX
+    1) convert the cert and key to a p12 file
+       via: openssl pkcs12 -export -in ./<client_cert> -inkey ./<client_key> -out client.p12
+       openssl pkcs12 -export -in ./acct-mgt-2.crt -inkey ./acct-mgt-2.key -out acct-mgt-2.p12
+
+    2) call curl with
+       curl -v -kv -E ./client.p12:password http://url...
+
+    3) auth_opts can be of the following:
+
+          auth_opts = ["-E","./client_cert/acct-mgt-2.crt", "-key", "./client_cert/acct-mgt-2.key"]
+          auth_opts = ["-cert", r"acct-mgt-2",]
+
+ Initial test to confirm that something is working
+    curl -kv https://acct-mgt.apps.cnv.massopen.cloud/projects/acct-mgt
+    curl -u <user>:<password> -kv https://acct-mgt.apps.cnv.massopen.cloud/projects/acct-mgt
+
+  -- testing with no authentication:
+     python3 -m pytest acct-mgt-test.py --amurl http://am2.apps.cnv.massopen.cloud
+
+  -- testing with basic authentication
+     python3 -m pytest acct-mgt-test.py --amurl https://acct-mgt.massopen.cloud --basic "user:pass"
+"""
 import subprocess
 import re
 import time
 import json
 import pprint
-import pytest
+
+# import pytest
 import pytest_check as check
 
 
 def get_pod_status(project, pod_name):
+    """This gets the specified pod status from openshift"""
     result = subprocess.run(
         ["oc", "-n", project, "-o", "json", "get", "pod", pod_name],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        check=True,
     )
     if result.returncode == 0:
         result_json = json.loads(result.stdout.decode("utf-8"))
@@ -50,6 +61,11 @@ def get_pod_status(project, pod_name):
 #   project,pod_name: identify the pod
 #   statuses: the array of statuses to wait for
 def wait_while(project, pod_name, statuses, time_out=300):
+    """
+    This basically waits while a pod is in a set of statuses and possibly times out
+    It is used to wait for openshift to do something with the pod, as in, deleting
+    or starting a pod.
+    """
     time_left = time_out
     time_interval = 5
     time.sleep(time_interval)
@@ -65,7 +81,11 @@ def wait_while(project, pod_name, statuses, time_out=300):
 
 
 def wait_until_done(oc_cmd, finished_pattern, time_out=30, decrement=5):
-    p1 = re.compile(finished_pattern)
+    """
+    This wait while an oc command is running, looking for a pattern that
+    indicates it is finished.
+    """
+    pattern1 = re.compile(finished_pattern)
     done = False
     oc_array = oc_cmd.split(" ")
     matched_line = ""
@@ -73,67 +93,45 @@ def wait_until_done(oc_cmd, finished_pattern, time_out=30, decrement=5):
         time.sleep(5)
         time_out = time_out - decrement
         result = subprocess.run(
-            oc_array, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+            oc_array, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True
         )
-        lines = result.stdout.decode("utf-8").split("\n")
-        cnt = 0
-        for l in lines:
-            if p1.match(l):
-                matched_line = l
+        line_list = result.stdout.decode("utf-8").split("\n")
+        for line in line_list:
+            if pattern1.match(line):
+                matched_line = line
                 done = True
-    if p1.match(matched_line):
+    if pattern1.match(matched_line):
         return True
     return False
 
 
-def user(user_name, op, success_pattern, auth_opts=[]):
-    if op == "check":
-        url_op = "GET"
-    elif op == "add":
-        url_op = "PUT"
-    elif op == "del":
-        url_op = "DELETE"
-
-    # result = subprocess.run(
-    #   ["curl", "-X", op, "-kv", "-E","./client_cert/acct-mgt-2.crt", "-key", "./client_cert/acct-mgt-2.key", url + "/users/" + user_name],
-    #   ["curl", "-X", op, "-kv", "-cert", r"acct-mgt-2", url + "/users/" + user_name],
-    #    stdout=subprocess.PIPE,
-    #    stderr=subprocess.STDOUT,
-    # )
-    cmd = ["curl", "-X", op, "-kv"] + auth_opts + [url + "/users/" + user_name]
-    result = subprocess.run(
-        cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-    )
-
-
 def compare_results(result, pattern):
+    """This compares the result of a subprocess (usually curl) call  with a pattern"""
     if result is not None:
-        p1 = re.compile(pattern)
-        lines = result.stdout.decode("utf-8").split("\n")
-        cnt = 0
-        for l in lines:
-            # print("line ==> " + l)
-            # print("patt --> " + pattern)
-            if p1.match(l):
+        pattern1 = re.compile(pattern)
+        line_array = result.stdout.decode("utf-8").split("\n")
+        for line in line_array:
+            if pattern1.match(line):
                 return True
     return False
 
 
 def oc_resource_exist(resource, kind, name, project=None):
+    """This uses oc to determin if an openshift resource exists"""
     result = None
     if project is None:
         result = subprocess.run(
             ["oc", "-o", "json", "get", resource, name],
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
+            check=True,
         )
     else:
         result = subprocess.run(
             ["oc", "-o", "json", "-n", project, "get", resource, name],
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
+            check=True,
         )
     if result.returncode == 0:
         if result.stdout is not None:
@@ -143,7 +141,8 @@ def oc_resource_exist(resource, kind, name, project=None):
     return False
 
 
-def ms_check_project(acct_mgt_url, project_name, auth_opts=[]):
+def ms_check_project(acct_mgt_url, project_name, auth_opts=None):
+    """Checks if the project exists using the microserver"""
     cmd = (
         ["curl", "-X", "GET", "-kv"]
         + auth_opts
@@ -153,6 +152,7 @@ def ms_check_project(acct_mgt_url, project_name, auth_opts=[]):
         cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        check=True,
     )
     pprint.pprint(result)
     return compare_results(
@@ -160,18 +160,20 @@ def ms_check_project(acct_mgt_url, project_name, auth_opts=[]):
     )
 
 
-# expect this to be called with
-#  project_uuid="1234-1234-1234-1234"
-#  displayNameStr=None | '{"displayName":"project_name"}' | '{"funkyName":"project_name"}'
-#
-# examples:
-#   curl -kv http://am2.apps.cnv.massopen.cloud/projects/test-001
-#   curl -X PUT -kv http://am2.apps.cnv.massopen.cloud/projects/test-001
-#
-#
-def ms_create_project(acct_mgt_url, project_uuid, displayNameStr, auth_opts=[]):
+def ms_create_project(acct_mgt_url, project_uuid, display_name_str, auth_opts=None):
+    """s
+    This creates a project (namespace) within openshift using the microserver
+
+    expect this to be called with
+    project_uuid="1234-1234-1234-1234"
+    displayNameStr=None | '{"displayName":"project_name"}' | '{"funkyName":"project_name"}'
+
+    examples:
+        curl -kv http://am2.apps.cnv.massopen.cloud/projects/test-001
+        curl -X PUT -kv http://am2.apps.cnv.massopen.cloud/projects/test-001
+    """
     cmd = ""
-    if displayNameStr is None:
+    if display_name_str is None:
         cmd = (
             [
                 "curl",
@@ -190,7 +192,7 @@ def ms_create_project(acct_mgt_url, project_uuid, displayNameStr, auth_opts=[]):
                 "PUT",
                 "-kv",
                 "-d",
-                displayNameStr,
+                display_name_str,
             ]
             + auth_opts
             + [acct_mgt_url + "/projects/" + project_uuid]
@@ -199,13 +201,15 @@ def ms_create_project(acct_mgt_url, project_uuid, displayNameStr, auth_opts=[]):
         cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        check=True,
     )
     return compare_results(
         result, r'{"msg": "project created \(' + project_uuid + r'\)"}'
     )
 
 
-def ms_delete_project(acct_mgt_url, project_name, auth_opts=[]):
+def ms_delete_project(acct_mgt_url, project_name, auth_opts=None):
+    """This deletes the specified project(namespace) using the microserver"""
     cmd = (
         ["curl", "-X", "DELETE", "-kv"]
         + auth_opts
@@ -215,13 +219,15 @@ def ms_delete_project(acct_mgt_url, project_name, auth_opts=[]):
         cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        check=True,
     )
     return compare_results(
         result, r'{"msg": "project deleted \(' + project_name + r'\)"}'
     )
 
 
-def ms_check_user(acct_mgt_url, user_name, auth_opts=[]):
+def ms_check_user(acct_mgt_url, user_name, auth_opts=None):
+    """This checks if a user exists using the microserver"""
     cmd = (
         [
             "curl",
@@ -236,23 +242,26 @@ def ms_check_user(acct_mgt_url, user_name, auth_opts=[]):
         cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        check=True,
     )
     return compare_results(result, r'{"msg": "user \(' + user_name + r'\) exists"}')
 
 
-def ms_create_user(acct_mgt_url, user_name, auth_opts=[]):
-
+def ms_create_user(acct_mgt_url, user_name, auth_opts=None):
+    """Creates a user using the microserver"""
     result = subprocess.run(
         ["curl", "-X", "PUT", "-kv"]
         + auth_opts
         + [acct_mgt_url + "/users/" + user_name],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        check=True,
     )
     return compare_results(result, r'{"msg": "user created \(' + user_name + r'\)"}')
 
 
-def ms_delete_user(acct_mgt_url, user_name, auth_opts=[]):
+def ms_delete_user(acct_mgt_url, user_name, auth_opts=None):
+    """Deletes a user usering the microserver"""
     cmd = (
         ["curl", "-X", "DELETE", "-kv"]
         + auth_opts
@@ -262,6 +271,7 @@ def ms_delete_user(acct_mgt_url, user_name, auth_opts=[]):
         cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        check=True,
     )
     return compare_results(result, r'{"msg": "user deleted \(' + user_name + r'\)"}')
 
@@ -272,8 +282,9 @@ def ms_user_project_get_role(
     project_name,
     role,
     success_pattern,
-    auth_opts=[],
+    auth_opts=None,
 ):
+    """Gets user roles from projects using the microserver"""
     cmd = (
         [
             "curl",
@@ -296,14 +307,16 @@ def ms_user_project_get_role(
         cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        check=True,
     )
     print("get role --> result: " + result.stdout.decode("utf-8") + "\n\n")
     return compare_results(result, success_pattern)
 
 
 def ms_user_project_add_role(
-    acct_mgt_url, user_name, project_name, role, success_pattern, auth_opts=[]
+    acct_mgt_url, user_name, project_name, role, success_pattern, auth_opts=None
 ):
+    """adds user roles to a project using the microserver"""
     cmd = (
         ["curl", "-X", "PUT", "-kv"]
         + auth_opts
@@ -321,14 +334,16 @@ def ms_user_project_add_role(
         cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        check=True,
     )
     print("add role --> result: " + result.stdout.decode("utf-8") + "\n\n")
     return compare_results(result, success_pattern)
 
 
 def ms_user_project_remove_role(
-    acct_mgt_url, user_name, project_name, role, success_pattern, auth_opts=[]
+    acct_mgt_url, user_name, project_name, role, success_pattern, auth_opts=None
 ):
+    """removes user role from a project using the microserver"""
     cmd = (
         ["curl", "-X", "DELETE", "-kv"]
         + auth_opts
@@ -346,14 +361,23 @@ def ms_user_project_remove_role(
         cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        check=True,
     )
     return compare_results(result, success_pattern)
 
 
 def test_project(acct_mgt_url, auth_opts):
-    result = 0
-    # if(oc_resource_exist("project", "test-001",'test-001[ \t]*test-001[ \t]','Error from server (NotFound): namespaces "test-001" not found')):
-    #    print("Error: test_project failed as a project with a name of test-001 exists.  Please delete first and rerun the tests\n")
+    """This tests project existence/creation/deletion"""
+    # if oc_resource_exist(
+    #    "project",
+    #    "test-001",
+    #    "test-001[ \t]*test-001[ \t]",
+    #    'Error from server (NotFound): namespaces "test-001" not found',
+    # ):
+    #    print(
+    #        "Error: test_project failed as a project with a name of test-001 exists.  \n" +
+    #        "Please delete first and rerun the tests\n"
+    #    )
     #    assertTrue(False)
 
     # test if project doesn't exist
@@ -423,7 +447,8 @@ def test_project(acct_mgt_url, auth_opts):
     )
 
     # these tests are primarily done to ensure that the microserver doesn't crash
-    #    When the "displayName" is not present, or the json doesn't exist, the displayName shall default to the project_uuid (first parameter)
+    #    When the "displayName" is not present, or the json doesn't exist, the displayName shall
+    #    default to the project_uuid (first parameter)
     check.is_true(
         ms_create_project(
             acct_mgt_url,
@@ -454,8 +479,17 @@ def test_project(acct_mgt_url, auth_opts):
 
 
 def test_user(acct_mgt_url, auth_opts):
-    # if(oc_resource_exist("user", "test01",r'test01[ \t]*[a-f0-9\-]*[ \t]*sso_auth:test01',r'Error from server (NotFound): users.user.openshift.io "test01" not found')):
-    #    print("Error: test_user failed as a user with a name of test01 exists.  Please delete first and rerun the tests\n")
+    """This tests user (and user identity) existence/creation/deletion"""
+    # if oc_resource_exist(
+    #    "user",
+    #    "test01",
+    #    r"test01[ \t]*[a-f0-9\-]*[ \t]*sso_auth:test01",
+    #    r'Error from server (NotFound): users.user.openshift.io "test01" not found',
+    # ):
+    #    print(
+    #        "Error: test_user failed as a user with a name of test01 exists.\n" +
+    #        "Please delete first and rerun the tests\n"
+    #    )
     #    assertTrue(False)
 
     check.is_false(
@@ -464,7 +498,7 @@ def test_user(acct_mgt_url, auth_opts):
     )
 
     # test user creation
-    # test01                    bfd6dab5-11f3-11ea-89a6-fa163e2bb38b                         sso_auth:test01
+    # test01      bfd6dab5-11f3-11ea-89a6-fa163e2bb38b      sso_auth:test01
     if not oc_resource_exist("users", "User", "test01"):
         check.is_true(
             ms_create_user(acct_mgt_url, "test01", auth_opts),
@@ -519,6 +553,7 @@ def test_user(acct_mgt_url, auth_opts):
 
 
 def test_project_user_role(acct_mgt_url, auth_opts):
+    """This tests project role existence/creation/update/deletion"""
     # Create a project
     if not oc_resource_exist("project", "Project", "test-002"):
         check.is_true(
@@ -533,15 +568,15 @@ def test_project_user_role(acct_mgt_url, auth_opts):
     )
 
     # Create some users test02 - test-05
-    for x in range(2, 6):
-        if not oc_resource_exist("users", "User", "test0" + str(x)):
+    for user_number in range(2, 6):
+        if not oc_resource_exist("users", "User", "test0" + str(user_number)):
             check.is_true(
-                ms_create_user(acct_mgt_url, "test0" + str(x), auth_opts),
-                "Unable to create user " + "test0" + str(x),
+                ms_create_user(acct_mgt_url, "test0" + str(user_number), auth_opts),
+                "Unable to create user " + "test0" + str(user_number),
             )
         check.is_true(
-            oc_resource_exist("users", "User", "test0" + str(x)),
-            "user test0" + str(x) + " not found",
+            oc_resource_exist("users", "User", "test0" + str(user_number)),
+            "user test0" + str(user_number) + " not found",
         )
 
     # now bind an admin role to the user
@@ -620,15 +655,13 @@ def test_project_user_role(acct_mgt_url, auth_opts):
 
     # Clean up by removing the users and project (test-002)
     check.is_true(
-        ms_delete_project(acct_mgt_url, "test-002", auth_opts) == True,
+        ms_delete_project(acct_mgt_url, "test-002", auth_opts) is True,
         "project (test-002) deleted",
     )
-    for x in range(2, 6):
-        if oc_resource_exist("users", "User", "test0" + str(x)):
+    for user_number in range(2, 6):
+        if oc_resource_exist("users", "User", "test0" + str(user_number)):
             check.is_true(
-                ms_delete_user(acct_mgt_url, "test0" + str(x), auth_opts) == True,
-                "user " + "test0" + str(x) + "unable to be deleted",
+                ms_delete_user(acct_mgt_url, "test0" + str(user_number), auth_opts)
+                is True,
+                "user " + "test0" + str(user_number) + "unable to be deleted",
             )
-
-
-# def test_quota(self):
